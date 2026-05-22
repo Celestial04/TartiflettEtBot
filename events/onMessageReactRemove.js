d082810aa6949cf86408466e92109de56ba7b851
Celestial04 <perso@katsuu04.dev>
2026-05-22 12:44:06 +0200

    🐛 fix(onMessageReactRemove): correct emoji logging and timestamp retrieval
    - change emoji logging to display the entire emoji object instead of just the name
    - fix timestamp retrieval by using emoji's createdTimestamp instead of creation time

--- events/onMessageReactRemove.js
+++ events/onMessageReactRemove.js
@@ -6,7 +6,7 @@ module.exports = {
     once: false,
     async execute(msgReact, User, msgReactDetails) {
 
-            console.log(msgReact.emoji.name)
+            console.log(msgReact.emoji)
         if (msgReact.message.author.bot === false) {
             console.log(`➖💬 ${User.name} → ${msgReact.message.channel.name}: "${msgReact.message.content}" "${msgReact.message.content}" [${moment().format(('MMMM Do YYYY, h:mm:ss a'))}]`);
             const targetChannel = await msgReact.client.channels.fetch('1504030484752240823');
@@ -17,7 +17,7 @@ module.exports = {
                 .setDescription(msgReact.emoji.name + ' à été retiré par <@' + User.id + '> sur le message de <@' + msgReact.message.author.id + '> disant:\n' + msgReact.message.content)
                 .setThumbnail('https://i.imgur.com/AfFp7pu.png')
                 .addFields(
-                    { name: 'Créé', value: ' <t:' + Math.floor(msgReact.createdAt) + ':R>.', inline: true },
+                    { name: 'Créé', value: ' <t:' + msgReact.emoji.createdTimestamp + ':R>.', inline: true },
                     msgReact? { name: 'ID du message', value: msgReact.message.id, inline: true } : { name: 'ID', value: msgReact.id, inline: true },
                     { name: 'Retiré dans', value: '<#' + msgReact.message.channelId + '> (' + msgReact.message.channel.name + ')', inline: true },
                 )
